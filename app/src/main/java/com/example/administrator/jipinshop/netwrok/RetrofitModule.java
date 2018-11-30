e6286cdddcec7de79595125a0d69baf3238c3a60
moxiaoting <moxiaoting163@163.com>
2018-11-30 20:08:14 +0800

    优化

--- app/src/main/java/com/example/administrator/jipinshop/netwrok/RetrofitModule.java
+++ app/src/main/java/com/example/administrator/jipinshop/netwrok/RetrofitModule.java
@@ -15,7 +15,7 @@ public class RetrofitModule {
 //    public static final String URL = "https://api.jipincheng.cn/";//正式地址
     public static final String URL = "http://192.168.1.48:8081/";//测试地址
     public static final String UP_BASE_URL = URL;
-    public static final String H5_URL = "http://www.jipincheng.cn/";
+    public static final String H5_URL = "https://www.jipincheng.cn/";
 
     @ApplicationScope
     @Provides
