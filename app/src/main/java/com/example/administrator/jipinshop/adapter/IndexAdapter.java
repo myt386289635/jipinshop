a00030aaf017b295e6426c576d2d7d68010498ec
moxiaoting <moxiaoting163@163.com>
2019-05-30 16:11:13 +0800

    优化引导页+添加360首发欢迎页

--- app/src/main/java/com/example/administrator/jipinshop/adapter/IndexAdapter.java
+++ app/src/main/java/com/example/administrator/jipinshop/adapter/IndexAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.administrator.jipinshop.R;
@@ -48,7 +49,8 @@ public  class IndexAdapter  extends PagerAdapter {
     public Object instantiateItem(ViewGroup container, int position) {
         View view = LayoutInflater.from(mContext).inflate(R.layout.item_index, container, false);
         ImageView imageView = view.findViewById(R.id.recommend_img_rotate);
-        TextView textView = view.findViewById(R.id.index_into);
+        ImageView textView = view.findViewById(R.id.index_into);
+        RelativeLayout relativeLayout = view.findViewById(R.id.Container);
         imageView.setImageResource(mList.get(position));
         if(position == 3){
             textView.setVisibility(View.VISIBLE);
@@ -59,6 +61,20 @@ public  class IndexAdapter  extends PagerAdapter {
             });
         }else {
             textView.setVisibility(View.GONE);
+        }
+        switch (position){
+            case 0:
+                relativeLayout.setBackgroundResource(R.color.color_FD7F2A);
+                break;
+            case 1:
+                relativeLayout.setBackgroundResource(R.color.color_67B6F7);
+                break;
+            case 2:
+                relativeLayout.setBackgroundResource(R.color.color_F76667);
+                break;
+            case 3:
+                relativeLayout.setBackgroundResource(R.color.color_4FCACB);
+                break;
         }
         container.addView(view);
         return view;
