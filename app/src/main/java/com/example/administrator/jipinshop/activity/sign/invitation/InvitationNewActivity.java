d9bf1aa2de3da49480fbbaa0ef2c4451e5c5d291
moxiaoting <moxiaoting163@163.com>
2019-06-11 12:08:16 +0800

    优化邀请页面逻辑

--- app/src/main/java/com/example/administrator/jipinshop/activity/sign/invitation/InvitationNewActivity.java
+++ app/src/main/java/com/example/administrator/jipinshop/activity/sign/invitation/InvitationNewActivity.java
@@ -1,15 +1,20 @@
 package com.example.administrator.jipinshop.activity.sign.invitation;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 
 import com.example.administrator.jipinshop.R;
 import com.example.administrator.jipinshop.adapter.InvitationNewAdapter;
 import com.example.administrator.jipinshop.base.BaseActivity;
 import com.example.administrator.jipinshop.databinding.ActivityInvitation2Binding;
+import com.example.administrator.jipinshop.util.ShareUtils;
 import com.example.administrator.jipinshop.view.viewpager.transformer.ScalePagerTransformer;
+import com.umeng.socialize.UMShareAPI;
+import com.umeng.socialize.bean.SHARE_MEDIA;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,11 +24,12 @@ import java.util.List;
  * @create 2019/6/10
  * @Describe 邀请好友页面更改
  */
-public class InvitationNewActivity extends BaseActivity implements View.OnClickListener {
+public class InvitationNewActivity extends BaseActivity implements View.OnClickListener, ViewPager.OnPageChangeListener {
 
     private ActivityInvitation2Binding mBinding;
     private InvitationNewAdapter mAdapter;
     private List<String> mList;
+    private int position = 0;//当前是第几个位置，默认为第一个
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -51,6 +57,7 @@ public class InvitationNewActivity extends BaseActivity implements View.OnClickL
         });
         ScalePagerTransformer transformer = new ScalePagerTransformer();
         mBinding.viewPager.setPageTransformer(true,transformer);
+        mBinding.viewPager.addOnPageChangeListener(this);
     }
 
     @Override
@@ -59,6 +66,45 @@ public class InvitationNewActivity extends BaseActivity implements View.OnClickL
             case R.id.title_back:
                 finish();
                 break;
+            case R.id.share_wechat:
+                new ShareUtils(this, SHARE_MEDIA.WEIXIN)
+                        .shareImage(this, mList.get(position));
+                break;
+            case R.id.share_pyq:
+                new ShareUtils(this, SHARE_MEDIA.WEIXIN_CIRCLE)
+                        .shareImage(this, mList.get(position));
+                break;
+            case R.id.share_weibo:
+                new ShareUtils(this, SHARE_MEDIA.SINA)
+                        .shareImage(this, mList.get(position));
+                break;
         }
+    }
+
+    @Override
+    public void onPageScrolled(int i, float v, int i1) {
+
+    }
+
+    @Override
+    public void onPageSelected(int i) {
+        position = i;
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int i) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        UMShareAPI.get(this).release();
+        super.onDestroy();
     }
 }
