a2f7f26ffdec2b6d5a2508abd00caaf5c5346ccf
moxiaoting <moxiaoting163@163.com>
2019-09-19 15:45:33 +0800

    优化

--- app/src/main/java/com/example/administrator/jipinshop/activity/wellcome/WellComeActivity.java
+++ app/src/main/java/com/example/administrator/jipinshop/activity/wellcome/WellComeActivity.java
@@ -50,7 +50,7 @@ public class WellComeActivity extends BaseActivity implements WellComeView {
     }
 
 
-    CountDownTimer timer = new CountDownTimer(1 * 1000, 1000) {
+    CountDownTimer timer = new CountDownTimer(1500, 500) {
         @Override
         public void onTick(long millisUntilFinished) {
 
