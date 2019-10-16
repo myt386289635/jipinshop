bb1b514f80cc106b09972a7711e8583138464cba
moxiaoting <moxiaoting163@163.com>
2019-10-16 15:09:05 +0800

    自定义吐司

--- app/src/main/java/com/example/administrator/jipinshop/util/ToastUtil.java
+++ app/src/main/java/com/example/administrator/jipinshop/util/ToastUtil.java
@@ -2,6 +2,9 @@ package com.example.administrator.jipinshop.util;
 
 import android.content.Context;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import com.blankj.utilcode.util.ToastUtils;
 import com.example.administrator.jipinshop.R;
@@ -14,15 +17,18 @@ import com.example.administrator.jipinshop.R;
 public class ToastUtil {
 
     public static void show(String info){
-        ToastUtils.setBgResource(-1);
+        ToastUtils.setView(null);
         ToastUtils.setGravity(Gravity.CENTER, 0, 0);
         ToastUtils.showShort(info);
     }
 
     public static void showTop(String info, Context context){
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View toastRoot = inflater.inflate(R.layout.toast, null);
+        TextView toast_text = toastRoot.findViewById(R.id.toast_text);
+        toast_text.setText(info);
         int yOffset = (int) context.getResources().getDimension(R.dimen.y166);
-        ToastUtils.setBgResource(R.drawable.bg_toast);
         ToastUtils.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.TOP, 0, yOffset);
-        ToastUtils.showShort(info);
+        ToastUtils.showCustomShort(toastRoot);
     }
 }
