b9f83e33ebf36f623f9a8f6ead75b14b649f86c9
moxiaoting <moxiaoting163@163.com>
2019-10-25 16:25:04 +0800

    修复小程序无法打开安卓app的问题

--- app/src/main/java/com/example/administrator/jipinshop/wxapi/WXEntryActivity.java
+++ app/src/main/java/com/example/administrator/jipinshop/wxapi/WXEntryActivity.java
@@ -1,5 +1,16 @@
 package com.example.administrator.jipinshop.wxapi;
 
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.administrator.jipinshop.R;
+import com.example.administrator.jipinshop.activity.wellcome.WellComeActivity;
+import com.tencent.mm.opensdk.constants.ConstantsAPI;
+import com.tencent.mm.opensdk.modelbase.BaseReq;
+import com.tencent.mm.opensdk.modelbase.BaseResp;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
 import com.umeng.socialize.weixin.view.WXCallbackActivity;
 
 /**
@@ -7,5 +18,43 @@ import com.umeng.socialize.weixin.view.WXCallbackActivity;
  * @create 2018/8/13
  * @Describe
  */
-public class WXEntryActivity extends WXCallbackActivity {
+public class WXEntryActivity extends WXCallbackActivity implements IWXAPIEventHandler {
+
+    private IWXAPI api;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wx);
+        api = WXAPIFactory.createWXAPI(this, null);
+        api.handleIntent(getIntent(), this);
+
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        api.handleIntent(intent, this);
+    }
+
+
+    //微信发送的请求将回调到 onReq 方法
+    @Override
+    public void onReq(BaseReq baseReq) {
+        switch (baseReq.getType()){
+            case ConstantsAPI.COMMAND_SHOWMESSAGE_FROM_WX:
+                finish();
+                Intent intent = new Intent(this, WellComeActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    //发送到微信请求的响应结果将回调到 onResp 方法
+    @Override
+    public void onResp(BaseResp baseResp) {
+
+    }
 }
